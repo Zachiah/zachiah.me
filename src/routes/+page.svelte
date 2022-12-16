494e2597f5cf869710380be35f672d7a07971e85
Zachiah Sawyer <zachiah@proton.me>
2022-12-16 07:51:25 -0800

    feat: add basic scaffolding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import ResponsiveImage from './ResponsiveImage.svelte';
+</script>
+
+<div class="wrapper">
+	<section>
+		<div>
+			<h1>Hi my name is Zachiah!</h1>
+			<div class="image-container">
+				<ResponsiveImage src="/pfp.jpg" alt="A picture of Zachiah Sawyer" />
+			</div>
+		</div>
+	</section>
+
+    <section>
+        <div></div>
+    </section>
+</div>
+
+<style>
+	:global(*) {
+		box-sizing: border-box;
+		padding: 0;
+		margin: 0;
+		font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode',
+			Geneva, Verdana, sans-serif;
+		font-size: 16px;
+		font-weight: normal;
+		color: currentColor;
+		text-decoration: none;
+	}
+
+    .wrapper {
+        scroll-snap-type: y mandatory;
+        height: 100vh;
+        overflow: auto;
+    }
+
+    h1 {
+        font-size: 2rem;
+        margin: 1rem 0;
+    }
+	section {
+		height: 100%;
+		width: 100vw;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+        scroll-snap-align: start;
+	}
+
+	section > div {
+		padding: 2rem;
+        width: 50%;
+        max-width: 100%;
+	}
+    
+    .image-container {
+    }
+</style>
---
+++ src/routes/ResponsiveImage.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let src: string;
+    export let alt: string;
+</script>
+
+<img src={src} alt={alt} />
+
+<style>
+    img {
+        max-width: 100%;
+        width: 100%;
+        height: 100%;
+        max-height: 100%;
+        object-fit: contain;
+    }
+</style>
